abe465766d4d9d7b72fa4e284a25482ef250156c
klmeir <carlos.merino.iriarte@gmail.com>
2017-08-26 11:00:18 -0500

    Initial commit

---
+++ EdadLegal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EdadLegal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite su nombre y pulse la tecla Enter:");
+            string nombre = System.Console.ReadLine();
+            System.Console.WriteLine("Digite su edad y pulse la tecla Enter:");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Tu nombre es " + nombre + " y legalmente eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Tu nombre es " + nombre + " y legalmente NO eres mayor de edad");
+            }
+        }
+    }
+}
---
+++ Saludo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello " + args[0]);
+        }
+    }
+}
